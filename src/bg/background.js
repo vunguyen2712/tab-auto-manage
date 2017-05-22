fc0076892f9be9bd43cedc98ebf324b40802e34c
Vu Nguyen <vunguyen2712@gmail.com>
2017-05-21 23:05:30 -0700

    Remove tab by Id successfully at a pre-set time

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,13 +1,70 @@
+"use strict";
 // if you checked "fancy-settings" in extensionizr.com, uncomment this lines
 
-var settings = new Store("settings", {
-    "sample_setting": "This is how you use Store.js to remember values"
+// var settings = new Store("settings", {
+//     "sample_setting": "This is how you use Store.js to remember values"
+// });
+//
+//
+// //example of using a message handler from the inject scripts
+// chrome.extension.onMessage.addListener(
+//   function(request, sender, sendResponse) {
+//   	chrome.pageAction.show(sender.tab.id);
+//     sendResponse();
+// });
+//
+// chrome.tabs.getSelected(null, function(tab) {
+//     var tab = tab.id;
+//     tabUrl = tab.url;
+//     alert(tab.url);
+// });
+//
+
+/*
+  -- Extension setting --
+*/
+var timeToKeepInMinutes = 1/12; // double
+
+
+chrome.tabs.onCreated.addListener(function(tab) {
+    var tabId = tab.id;
+    var tabUrl = tab.url;
+    var idList = "";
+    var alarmName = "" + tabId;
+    // console.log("just created. Tab id: " + tabId);
+    chrome.alarms.create(alarmName, {delayInMinutes: timeToKeepInMinutes});
+    chrome.tabs.query({}, function(tabs){
+        for (var i = 0; i < tabs.length; i++) {
+            idList += tabs[i].id + " ";
+        }
+        // alert("Id list is:" + idList);
+    });
+
+    // alert("just created. Tab id: " + tab);
 });
 
+chrome.alarms.onAlarm.addListener(function(alarm){
+    closeTabOnAlarm(alarm.name);
+    clearAlarm(alarm.name);
+    console.log(alarm);
 
-//example of using a message handler from the inject scripts
-chrome.extension.onMessage.addListener(
-  function(request, sender, sendResponse) {
-  	chrome.pageAction.show(sender.tab.id);
-    sendResponse();
+});
+
+function closeTabOnAlarm(stringId) {
+  var intId = parseInt(stringId);
+  chrome.tabs.remove(intId, function(){
+    console.log("closing tab: " + intId);
   });
+}
+
+function clearAlarm(alarmName) {
+  chrome.alarms.clear(alarmName, function(){
+    console.log("Clearing alarm: " + alarmName);
+  });
+}
+
+chrome.browserAction.onClicked.addListener(function (tab) {
+    alert("Hey !!! You have clicked");
+});
+
+alert("background is running");
