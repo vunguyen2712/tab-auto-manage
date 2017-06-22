c5cb86ae3e78b38aa7ced5feca698a9e79d513fc
Vu Nguyen <vunguyen2712@gmail.com>
2017-06-22 02:08:59 -0700

    Remove tab data when clicking on a history link, update badge

--- src/extension-page/auto-closed-history.js
+++ src/extension-page/auto-closed-history.js
@@ -1,6 +1,9 @@
 angular.module('myApp', ['dataGrid', 'pagination', 'ngMaterial'])
     .controller('myAppController', ['$scope', 'myAppFactory', function ($scope, myAppFactory) {
 
+        var portToBg = chrome.extension.connect({
+               name: "CommunicationWithBg"
+        });
         $scope.autoClosedHistory = [];
         $scope.gridOptions = { // gridOptions data must be defined at least
             data: [],
@@ -16,6 +19,25 @@ angular.module('myApp', ['dataGrid', 'pagination', 'ngMaterial'])
                   sendResponse({recieveStatus: 'Successfully recieved data from backgroundjs'});
               }
         });
+
+        //@param: item (from the UI) is a tabInfo
+        $scope.openAutoClosedLink = function (item){
+            findAndRemoveOpenLink(item);
+        }
+
+        function findAndRemoveOpenLink(tabInfo){
+            for(var i = 0; i < $scope.gridOptions.data.length; ++i){
+                if($scope.gridOptions.data[i].tabId === tabInfo.tabId){
+                    console.log('Removing ' + tabInfo.tabUrl);
+                    $scope.gridOptions.data.splice(i, 1);
+                    var openLinkMsg = {
+                        action: 'openLink',
+                        updatedTabsData: $scope.gridOptions.data
+                    };
+                    portToBg.postMessage(openLinkMsg); // send to bg
+                }
+            }
+        }
 
         function init(){
             chrome.runtime.sendMessage({action: 'getInitData'}, function(response) {
